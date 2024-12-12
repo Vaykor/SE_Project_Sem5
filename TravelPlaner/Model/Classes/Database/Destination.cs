881c55bb4a70aad5a09f5415903079b55f8cccba
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2024-12-12 10:31:33 +0100

    Feature: VS project with mvc folder division. Basic classes for database

---
+++ TravelPlaner/Model/Classes/Database/Destination.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlaner.Model.Classes.Database
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ TravelPlaner/Model/Classes/Database/TripMemory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlaner.Model.Classes.Database
+{
+    public class TripMemory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Photo { get; set; } // can be null
+        public string? Note { get; set; } // can be null
+        public string? SongURL { get; set;} // can be null
+
+        //byte[] imageArray = System.IO.File.ReadAllBytes(@"image file path"); -> transforming image into byte array
+        //string base64ImageRepresentation = Convert.ToBase64String(imageArray); -> converting byte array into string
+        //var img = Image.FromStream(new MemoryStream(Convert.FromBase64String(base64String))); -> converting string back to image
+
+    }
+}
