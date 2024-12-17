d91f66a841adf9673dc0d26db94ba516d5ba2625
Le meme Man <ms306097@student.polsl.pl>
2024-12-17 16:02:56 +0100

    SQLMerge

---
+++ TravelPlaner/View/Forms/SegmentDetails.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TravelPlaner.View.Forms
+{
+    partial class SegmentDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Expenses";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(343, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Destinations";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(190, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Memories";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(17, 35);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(139, 503);
+            listView1.TabIndex = 3;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // SegmentDetails
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(listView1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "SegmentDetails";
+            Size = new Size(886, 688);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListView listView1;
+    }
+}
