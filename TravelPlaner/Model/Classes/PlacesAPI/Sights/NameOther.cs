476a6e3f1f50787948e16c9315808acc0fc88ed0
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2025-01-11 16:58:20 +0100

    Feature: Get recommended turist landmarks for a given location with API

---
+++ TravelPlaner/Model/Classes/PlacesAPI/Sights/NameOther.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlaner.Model.Classes.PlacesAPI.Sights
+{
+    public class NameOther
+    {
+        public string? old_name { get; set; }
+    }
+}
