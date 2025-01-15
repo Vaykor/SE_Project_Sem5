0fc7fc2439d404fa82a64ce94abf7496fa8f600f
Le meme Man <ms306097@student.polsl.pl>
2025-01-15 22:35:17 +0100

    TODO: zaimplementowac addery;wprowadzic final wersje GUI;poprawic collapsowanie w wyswietlaniu detaili;

---
+++ TravelPlaner/Migrations/20250113172748_help3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelPlaner.Migrations
+{
+    /// <inheritdoc />
+    public partial class help3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LengthOfStay",
+                table: "Destination",
+                newName: "NightsSpentThere");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NightsSpentThere",
+                table: "Destination",
+                newName: "LengthOfStay");
+        }
+    }
+}
---
+++ TravelPlaner/View/Forms/PopupForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelPlaner.View.Forms
+{
+    public partial class PopupForm : Form
+    {
+        public PopupForm()
+        {
+            InitializeComponent();
+        }
+        public PopupForm(bool success)
+        {
+            InitializeComponent();
+            if (success)
+            {
+                label1.Text = "Success! Your trip should now appear in the database.";
+            }
+            else
+            {
+                label1.Text = "The values which you have inserted are empty. Please correct them before submitting changes.";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
